115f2ae35cb73ea0c9af8c1a3ca05fe01a6e42d1
slane <slane@wp.pl>
2017-11-18 18:59:56 +0100

    Rozszerzenie testu funkcjonalnego, użycie szablonowych zmiennych w widoku, poprawa home_page.

--- functional_tests.py
+++ functional_tests.py
@@ -36,19 +36,23 @@ class NewVisitorTest(unittest.TestCase):
     
     table = self.browser.find_element_by_id('id_list_table')
     rows = table.find_elements_by_tag_name('tr')
-    self.assertTrue(
-        any(row.text == '1: Kupić pawie pióra' for row in rows),
-        "Nowy element nie znajduje się w tabeli!"
-    )
+    self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
 
     # Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadania.
-    # Magda wpisała "Użycie pawich piór do zrobienia przynęty" (Magda jest niezwykle skrupulatna).
-    self.fail('Zakończenie testu!')
+    # Magda wpisała "Użyć pawich piór do zrobienia przynęty" (Magda jest niezwykle skrupulatna).
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys('Użyć pawich piór do zrobienia przynęty')
+    inputbox.send_keys(Keys.ENTER)
 
     # Strona została ponownie uaktualniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia.
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertIn('1: Kupić pawie pióra', [row.text for row in rows])
+    self.assertIn('2: Użyć pawich piór do zrobienia przynęty', [row.text for row in rows])
 
     # Magda była ciekawa, czy witryna zapamięta jej listę. Zwróćiła uwagę na wygenerowany dla niej
     # unikatowy adres URL, obok którego znajduje się tekst z wyjaśnieniem.
+    self.fail('Zakończenie testu!')
 
     # Przechodzi pod podany adres URL i widzi wyświetloną swoją listę rzeczy do zrobienia.
 
