a44c752263f550d65ef922ab3aad24319882ce8b
slane <slane@wp.pl>
2017-11-18 19:52:32 +0100

    Model dla obiektów Items oraz powiązane z nim migracje.

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
  
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
  
@@ -17,3 +18,21 @@ class HomePageTest(TestCase):
     def test_home_page_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'Nowy element listy'})
         self.assertIn('Nowy element listy', response.content.decode())
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'Absolutnie pierwszy element listy'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Drugi element listy'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'Absolutnie pierwszy element listy')
+        self.assertEqual(second_saved_item.text, 'Drugi element listy')
+
