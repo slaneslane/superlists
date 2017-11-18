dbb9a85b6552d87416556532ec2ced3a807fad2d
slane <slane@wp.pl>
2017-11-18 17:12:16 +0100

    Poprawienie testów jednostkowych w związku z wersją Django 1.10 a nie 1.08 jak w książce. Dodanie tokentu csrf do formularza.

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,6 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-from django.template.loader import render_to_string
  
 from lists.views import home_page
 
@@ -11,8 +10,15 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'Nowy element listy'
+
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+
+        self.assertIn('Nowy element listy', response.content.decode())
