678c71da64bf802c229c0447f9ed1c643608b413
slaneslane <slane@wp.pl>
2018-04-18 12:43:50 +0200

    Poprawiona literówka dla poprzedniego commita. 'Expected Conditions title is' zamiast 'staleness_of(old_page)'

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 #for fixing the selenium 3 problem with reloading waiting page at the end of 5th chapter:
 from contextlib import contextmanager
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support.expected_conditions import staleness_of
+from selenium.webdriver.support import expected_conditions as EC
 
 import unittest
 
@@ -27,10 +27,9 @@ class NewVisitorTest(LiveServerTestCase):
     self.assertIn(row_text, [row.text for row in rows])
 
   @contextmanager
-  def wait_for_page_load(self, timeout=5):
-    old_page = self.browser.find_element_by_tag_name("html")
+  def wait_for_page_load(self, pageTitle='', timeout=5):
     yield WebDriverWait(self, timeout).until(
-         staleness_of(old_page)
+        EC.title_is(pageTitle)
     )
 
 
@@ -61,7 +60,7 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox.send_keys(Keys.ENTER)
 
     # Wpisane przez Magde haslo pojawia sie na jej liscie.
-    with self.wait_for_page_load(timeout=2):
+    with self.wait_for_page_load('Lista rzeczy do zrobienia',timeout=2):
         self.check_for_row_in_list_table('1: Kupic pawie piora')
 
     # Lista Magdy ma swoj wlasny URL.
@@ -74,8 +73,8 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox.send_keys('Uzyc pawich pior do zrobienia przynety')
     inputbox.send_keys(Keys.ENTER)
 
-    # Strona zostala ponownie uaktualniona i teraz wyświetla dwa elementy na liscie rzeczy do zrobienia.
-    with self.wait_for_page_load(timeout=2):
+    # Strona zostala ponownie uaktualniona i teraz wyswietla dwa elementy na liscie rzeczy do zrobienia.
+    with self.wait_for_page_load('Lista rzeczy do zrobienia',timeout=2):
         self.check_for_row_in_list_table('1: Kupic pawie piora')
         self.check_for_row_in_list_table('2: Uzyc pawich pior do zrobienia przynety')
 
@@ -89,7 +88,7 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox.send_keys(Keys.ENTER)
 
     # Wpisane przez Magde haslo pojawia sie na jej liscie.
-    with self.wait_for_page_load(timeout=2):
+    with self.wait_for_page_load('Lista rzeczy do zrobienia',timeout=2):
         self.check_for_row_in_list_table('1: Kupic pawie piora')
 
     # Zauwaza, ze jej lista ma unikalny adres URL.
@@ -116,7 +115,7 @@ class NewVisitorTest(LiveServerTestCase):
     inputbox.send_keys(Keys.ENTER)
     
     # Wpisane przez Szymona haslo pojawia sie na jego liscie.
-    with self.wait_for_page_load(timeout=2):
+    with self.wait_for_page_load('Lista rzeczy do zrobienia',timeout=2):
         self.check_for_row_in_list_table('1: Kupic mleko')
 
     # Szymon otrzymuje unikatowy adres URL prowadzacy do listy.
