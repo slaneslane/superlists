ec52773d30c60d0bd1ee2114f0935c1808e80d1a
slaneslane <slane@wp.pl>
2018-04-19 06:06:28 +0000

    ALLOWED_HOSTS modified for pythonanywhere (still in debug). Wszystkie testy jednostkowe i funkcjonalne zaliczone na pythonanywhere.com :D )

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'qpurfz=xwdwv9rtdmht()irihr2rr0pcq63s_pxpaulq%u4yc!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'slane.pythonanywhere.com'
+]
 
 
 # Application definition
