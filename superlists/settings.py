024b7122a6719e325148b4ba6bc43d58a11965d8
slaneslane <slane@wp.pl>
2018-04-22 22:15:52 +0200

    Ustawienie STATIC_ROOT w pliku ustawień i wyłączenie funkcji admin.

--- superlists/settings.py
+++ superlists/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -119,5 +119,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
