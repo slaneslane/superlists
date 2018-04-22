aeb97200b52b2ddb1c0b253155bc3b27ab344f2f
slaneslane <slane@wp.pl>
2018-04-22 20:25:53 +0200

    Uzupełniony nagłówek dla 'base.html'. Test funkcjonalny dla wycentrowanego elementu - zaliczony (duże zmiany w stosunku do książki).

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -8,7 +8,7 @@ import time
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
   def setUp(self):
     # Rozpoczynamy nowa sesje przegladarki.
@@ -36,6 +36,12 @@ class NewVisitorTest(LiveServerTestCase):
                 raise e
             time.sleep(0.5 )
 
+  def element_centered(self, element_tag, delta_=10):
+    element = self.browser.find_element_by_id(element_tag)
+    browser_size = self.browser.get_window_size() 
+    self.assertAlmostEqual(
+        element.location['x'] + (element.size['width'] / 2), (browser_size['width'] / 2), 2, delta=delta_
+    )
 
   def test_can_start_a_list_for_one_user(self):
 
@@ -135,23 +141,18 @@ class NewVisitorTest(LiveServerTestCase):
 
     # Magda odwiedza strone glowna aplikacji.
     self.browser.get(self.live_server_url)
-    self.browser.set_window_size(1024, 768)
 
     # Zauwaza, ze inputbox znajduje sie na srodku strony.
-    inputbox = self.browser.find_element_by_id('id_new_item')
-    self.assertAlmostEqual(
-        inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
-    )
+    self.element_centered('id_new_item')
 
-    # Magda tworzy nowa liste i zauwaza, ze inputbox jest wciaz na srodku strony.
+    # Magda tworzy nowa liste
+    inputbox = self.browser.find_element_by_id('id_new_item')
     inputbox.send_keys('testowy')
     inputbox.send_keys(Keys.ENTER)
     self.wait_for_row_in_list_table_and_check_it('1: testowy')
 
-    inputbox = self.browser.find_element_by_id('id_new_item')
-    self.assertAlmostEqual(
-        inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
-    )
+    # Zauwaza, ze inputbox jest wciaz na srodku strony.
+    self.element_centered('id_new_item')
 
 
 
