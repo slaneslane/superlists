f65da799627f05b53fd030b5394d7fa2ef3a54d3
slaneslane <slane@wp.pl>
2018-04-28 17:42:52 +0200

    Uzupełnienie treści testu funkcjonalnego dla pustych elementów w liście. Dodanie oczekującej metody pomocniczej pobierającej funkcję jako argument.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -28,6 +28,16 @@ class FunctionalTest(StaticLiveServerTestCase):
     rows = table.find_elements_by_tag_name('tr')
     self.assertIn(row_text, [row.text for row in rows])
 
+  def wait_for(self, fn):
+    start_time = time.time()
+    while True:
+        try:
+            return fn()
+        except (AssertionError, WebDriverException) as e:
+            if time.time() - start_time > MAX_WAIT:
+                raise e
+            time.sleep(0.5 )
+
   def wait_for_row_in_list_table_and_check_it(self, row_text):
     start_time = time.time()
     while True:
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -2,21 +2,37 @@ from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
 
 
-
 class ItemValidationTest(FunctionalTest):
 
   def test_cannot_add_empty_list_items(self):
     # Magda przeszla na strone glowna i przypadkowo sprobowala utworzyc
     # pusty element na liscie. Nacisnala klawisz ENTER w pustym polu tekstowym.
+    self.browser.get(self.live_server_url)
+    self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
     # Po odswiezeniu strony glownej zobaczyla komunikat bledu
     # informujacy o niemozliwosci utworzenia pustego elementu na liscie.
+    self.wait_for(lambda: self.assertEqual(
+        self.browser.find_element_by_css_selector('.has-error').text,
+        "Element nie moze byc pusty!"
+    ))
 
     # Sprobowala ponownie wpisujac dowolny tekst i tym razem wszystko zadzialalo.
+    self.browser.find_element_by_id('id_new_item').send_keys('Kupic mleko')
+    self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table_and_check_it('1: Kupic mleko')
 
     # Przekornie po raz drugi sprobowala utworzyc pusty element na liscie.
+    self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
     # Na stronie listy otrzymala ostrzezenie podobne do wczesniejszego.
+    self.wait_for(lambda: self.assertEqual(
+        self.browser.find_element_by_css_selector('.has-error').text,
+        "Element nie moze byc pusty!"
+    ))
 
     # Element mogla poprawic wpisujac w nim dowolny tekst.
-    self.fail('Kontynuuj!')
+    self.browser.find_element_by_id('id_new_item').send_keys('Zrobic herbate')
+    self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table_and_check_it('1: Kupic mleko')
+    self.wait_for_row_in_list_table_and_check_it('2: Zrobic herbate')
