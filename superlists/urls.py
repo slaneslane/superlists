e70e9fb877dfb3729548083478eaf38f5c5e5b1d
slaneslane <slane@wp.pl>
2018-04-29 10:41:41 +0200

    Usunięcie portalu admina.

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
 from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/', include('lists.urls')),
-    #url(r'^admin/', admin.site.urls),
 ]
