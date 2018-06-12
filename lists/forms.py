3f3a08f2666b0aaca9cafd98e0f18bd1c6b876b5
slaneslane <31472808+slaneslane@users.noreply.github.com>
2018-06-12 09:13:08 +0200

    Mała zmiana kolejności zawartości klasy ItemForm.

--- lists/forms.py
+++ lists/forms.py
@@ -5,12 +5,7 @@ from django.core.exceptions import ValidationError
 EMPTY_LIST_ERROR = "Element listy nie może być pusty!" 
 DUPLICATE_ITEM_ERROR = "Ten element znajduje się już na liście"
 
-
 class ItemForm(forms.models.ModelForm):
-
-    def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
 
     class Meta:
         model = Item
@@ -27,7 +22,10 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_LIST_ERROR }
         }
-
+        
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
 
 class ExistingListItemForm(ItemForm):
 
