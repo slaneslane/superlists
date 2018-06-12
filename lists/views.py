1cff90458b0773c74a0748bd6e12af45248ab84f
slaneslane <31472808+slaneslane@users.noreply.github.com>
2018-06-12 09:07:53 +0200

    views.py - powrót do kudu opartego na funkcjach.

--- lists/views.py
+++ lists/views.py
@@ -1,39 +1,36 @@
-from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
-from django.views.generic import FormView, CreateView, DetailView
 from django.contrib.auth import get_user_model
 
-from lists.models import List
 from lists.forms import ItemForm, ExistingListItemForm
+from lists.models import List
 
 User = get_user_model()
 
-class HomePageView(FormView):
+def home_page(request):
+    return render(request, 'home.html', {'form': ItemForm()})
 
-    template_name = 'home.html'
-    form_class = ItemForm
-
-class NewListView(CreateView, HomePageView):
-
-    def form_valid(self, form):
+def new_list(request):
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
         list_ = List()
-        if self.request.user.is_authenticated:
-            list_.owner = self.request.user
+        if request.user.is_authenticated:
+            list_.owner = request.user
         list_.save()
         form.save(for_list=list_)
         return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
 
-class ViewAndAddToList(DetailView, CreateView):
-    model = List
-    template_name = 'list.html'
-    form_class = ExistingListItemForm
-
-    def get_form(self):
-        self.object = self.get_object() 
-        if self.request.method == "POST":
-            return self.form_class(for_list=self.object, data=self.request.POST)
-        return self.form_class(for_list=self.object)
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=list_)
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list_)
+    return render(request, 'list.html', {'list': list_, "form": form})
 
 def my_lists(request, email):
     owner = User.objects.get(email=email)
-    return render(request, 'my_lists.html', {'owner': owner})
+return render(request, 'my_lists.html', {'owner': owner})
