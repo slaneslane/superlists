edc28791d37a3608bab4f94718098ddd0c7d07ac
slaneslane <slane@wp.pl>
2018-06-18 09:15:12 +0200

    Dodanie do testu funkcjonalnego operacji wykonywania zrzutu ekranu w przypadku niepowodzenia testu.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,10 +4,14 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import os
 import time
+from datetime import datetime
 from .server_tools import reset_database
 
 MAX_WAIT = 10
 
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 # decorator function to wait:
 def wait(func):
@@ -22,7 +26,6 @@ def wait(func):
                 time.sleep(0.5)
     return modified_fn
 
-
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -33,8 +36,41 @@ class FunctionalTest(StaticLiveServerTestCase):
             reset_database(self.staging_server, os.environ.get('MY_SERVER_PORT'))
 
     def tearDown(self):
+        is self._test_has_failed():
+            if not os.path.exists(SCREEN_DUMP_LOCATION):
+                os.makedirs(SCREEN_DUMP_LOCATION)
+            for ix, handle in enumerate(self.browser.window_handles):
+                self._windowid = ix
+                self.browser.switch_to_window(handle)
+                self.take_screenshot()
+                self.dump_html()
         self.browser.quit()
+        super().tearDown()
     
+    def _test_has_failed(self):
+        return any(error for (method, error) in self._outcome.errors)
+
+    def _get_filename(self):
+        timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+        return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+            folder=SCREEN_DUMP_LOCATION,
+            classname=self.__class__.__name__,
+            method=self._testMethodName,
+            windowid=self._windowid,
+            timestamp=timestamp
+        )
+
+    def take_screenshot(self):
+        filename = self._get_filename() + '.png'
+        print('screenshotting to', filename)
+        self.browser.get_screenshot_as_file(filename)
+
+    def dump_html(self):
+        filename = self._get_filename() + '.html'
+        print('dumping page HTML to', filename)
+        with open(filename, 'w') as f:
+            f.write(self.browser.page_source)
+
     @wait
     def wait_for(self, fn):
         return fn()
