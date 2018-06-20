fb46c51c1b59937ac24d42de80ca63c5b604a420
slaneslane <slane@wp.pl>
2018-06-20 10:59:25 +0200

    Usunięcie zdublowanej funkcji newlist oraz dodanie get_object_or_404 jeśli w bazie danych brak listy lub użytkownika.

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth import get_user_model
 
 from lists.forms import ItemForm, ExistingListItemForm, NewListForm
@@ -14,18 +14,10 @@ def new_list(request):
     if form.is_valid():
         list_ = form.save(owner=request.user)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
-
-def new_list(request):
-    form = NewListForm(data=request.POST)
-    if form.is_valid():
-        list_ = form.save(owner=request.user)
-        return redirect(list_)
     return render(request, 'home.html', {'form': form})
 
 def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
+    list_ = get_object_or_404(List, id=list_id)
     form = ExistingListItemForm(for_list=list_)
     if request.method == 'POST':
         form = ExistingListItemForm(for_list=list_, data=request.POST)
@@ -35,5 +27,5 @@ def view_list(request, list_id):
     return render(request, 'list.html', {'list': list_, "form": form})
 
 def my_lists(request, email):
-    owner = User.objects.get(email=email)
+    owner = get_object_or_404(User, email=email)
     return render(request, 'my_lists.html', {'owner': owner})
