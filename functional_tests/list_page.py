8db43e521c9bc44e9f4079168805120fca55b8be
slaneslane <slane@wp.pl>
2018-06-23 18:02:16 +0200

    Dodanie .coverage do .gitignore. Poprawienie testu współdzielenia list.

--- functional_tests/list_page.py
+++ functional_tests/list_page.py
@@ -39,7 +39,7 @@ class ListPage(object):
         self.get_share_box().send_keys(Keys.ENTER)
         self.test.wait_for(lambda: self.test.assertIn(
             email,
-            [item.text] for item in self.get_shared_with_list()
+            [item.text for item in self.get_shared_with_list()]
         ))
 
     def get_list_owner(self):
